e96dbeb0eff14892aa60a9fa401ae33d4b0737cf
Jamie Leigh <jamie.leigh100@hotmail.co.uk>
2023-11-11 18:12:24 +0000

    Add file validation

--- client/src/components/FileImporter.jsx
+++ client/src/components/FileImporter.jsx
@@ -2,6 +2,8 @@ import React from 'react';
 import { useDropzone } from 'react-dropzone';
 
 const timeConverter = (time) => {
+  const re = /\d\d:\d\d:\d\d/g; // format: 00:00:00
+  if (!re.test(time)) return alert("Time format is incorrect. Please upload a CSV file exported from Toggl");
   const hms = time.split(':');
   const hours = parseInt(hms[0], 10) * 60;
   const minutes = parseInt(hms[1], 10);
@@ -14,7 +16,6 @@ const FileImporter = ({ setClients }) => {
   const tempClients = {};
 
   const onDrop = (acceptedFiles) => {
-    console.log('here')
     const file = acceptedFiles[0]; // only accept one file
     if (file.type !== "text/csv") return alert("Please upload a CSV file");
     const reader = new FileReader();
@@ -23,7 +24,8 @@ const FileImporter = ({ setClients }) => {
     reader.onload = () => {
       const binaryStr = reader.result;
       const lines = typeof binaryStr == 'string' ? binaryStr.split(/\r?\n/) : [];
-      lines.splice(0, 1); // remove header row
+      const headerLine = lines.splice(0, 1)[0]; // remove header row
+      if (headerLine !== "Project,Client,Description,Duration") return alert("Please upload a CSV file exported from Toggl");
       if (lines[lines.length - 1] === '') lines.pop(); // if last row is empty, remove it
       lines.forEach((line) => {
         const inputs = line.split(','); // get each cell
